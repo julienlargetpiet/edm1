3b3a97c576fd848928ead7d4d8208cbf49eb8eef
Julien <pgyygp090@gmail.com>
2024-08-20 18:18:22 +0200

    add function edm_arrangr

--- R/all_fun.R
+++ R/all_fun.R
@@ -17538,4 +17538,97 @@ sum_group2 <- function(inpt_datf, col_grp = c(), col_to_add = c()){
   return(rtn_datf)
 }
 
+#' edm_arranger
+#'
+#' Arranges data according to the values of a variable, see examples
+#'
+#' @param inpt_datf is the input dataframe
+#' @param col_order is the column names or the column number of the variable that will be used to arrange data
+#' @param top_n is the top values
+#'
+#' @examples
+#'
+#' print(edm_arrangr(inpt_datf = mtcars, col_order = "wt", top_n = 15, decreasing = FALSE))
+#' 
+#'                 mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+#' Lotus Europa   30.4   4  95.1 113 3.77 1.513 16.90  1  1    5    2
+#' Honda Civic    30.4   4  75.7  52 4.93 1.615 18.52  1  1    4    2
+#' Toyota Corolla 33.9   4  71.1  65 4.22 1.835 19.90  1  1    4    1
+#' Fiat X1-9      27.3   4  79.0  66 4.08 1.935 18.90  1  1    4    1
+#' Porsche 914-2  26.0   4 120.3  91 4.43 2.140 16.70  0  1    5    2
+#' Fiat 128       32.4   4  78.7  66 4.08 2.200 19.47  1  1    4    1
+#' Datsun 710     22.8   4 108.0  93 3.85 2.320 18.61  1  1    4    1
+#' Toyota Corona  21.5   4 120.1  97 3.70 2.465 20.01  1  0    3    1
+#' Mazda RX4      21.0   6 160.0 110 3.90 2.620 16.46  0  1    4    4
+#' Ferrari Dino   19.7   6 145.0 175 3.62 2.770 15.50  0  1    5    6
+#' Volvo 142E     21.4   4 121.0 109 4.11 2.780 18.60  1  1    4    2
+#' Mazda RX4 Wag  21.0   6 160.0 110 3.90 2.875 17.02  0  1    4    4
+#' Merc 230       22.8   4 140.8  95 3.92 3.150 22.90  1  0    4    2
+#' Ford Pantera L 15.8   8 351.0 264 4.22 3.170 14.50  0  1    5    4
+#' Merc 240D      24.4   4 146.7  62 3.69 3.190 20.00  1  0    4    2
+#' 
+#' 
+#' print(edm_arrangr(inpt_datf = mtcars, col_order = "wt", top_n = 10, decreasing = TRUE))
+#'                      mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+#' Lincoln Continental 10.4   8 460.0 215 3.00 5.424 17.82  0  0    3    4
+#' Chrysler Imperial   14.7   8 440.0 230 3.23 5.345 17.42  0  0    3    4
+#' Cadillac Fleetwood  10.4   8 472.0 205 2.93 5.250 17.98  0  0    3    4
+#' Merc 450SE          16.4   8 275.8 180 3.07 4.070 17.40  0  0    3    3
+#' Pontiac Firebird    19.2   8 400.0 175 3.08 3.845 17.05  0  0    3    2
+#' Camaro Z28          13.3   8 350.0 245 3.73 3.840 15.41  0  0    3    4
+#' Merc 450SLC         15.2   8 275.8 180 3.07 3.780 18.00  0  0    3    3
+#' Merc 450SL          17.3   8 275.8 180 3.07 3.730 17.60  0  0    3    3
+#' Duster 360          14.3   8 360.0 245 3.21 3.570 15.84  0  0    3    4
+#' Maserati Bora       15.0   8 301.0 335 3.54 3.570 14.60  0  1    5    8
+#' Dodge Challenger    15.5   8 318.0 150 2.76 3.520 16.87  0  0    3    2
+#'
+#' @export
+
+edm_arrangr <- function(inpt_datf, col_order, top_n = 10, decreasing = TRUE){
+  if (top_n > nrow(inpt_datf)){
+    top_n <- nrow(inpt_datf)
+  }
+  if (top_n < 1){
+    top_n <- 1
+  }
+  regex_spe_detect <- function(inpt){
+        fillr <- function(inpt_v, ptrn_fill="\\.\\.\\.\\d"){
+          ptrn <- grep(ptrn_fill, inpt_v)
+          while (length(ptrn) > 0){
+            ptrn <- grep(ptrn_fill, inpt_v)
+            idx <- ptrn[1] 
+            untl <- as.numeric(c(unlist(strsplit(inpt_v[idx], split="\\.")))[4]) - 1
+            pre_val <- inpt_v[(idx - 1)]
+            inpt_v[idx] <- pre_val
+            if (untl > 0){
+              for (i in 1:untl){
+                inpt_v <- append(inpt_v, pre_val, idx)
+              }
+            }
+          ptrn <- grep(ptrn_fill, inpt_v)
+          }
+          return(inpt_v)
+        }
+        inpt <- unlist(strsplit(x=inpt, split=""))
+        may_be_v <- c("[", "]", "{", "}", "-", "_", ".", "(", ")", "/", "%", "*", "^", "?", "$")
+        pre_idx <- unique(match(x=inpt, table=may_be_v))
+        pre_idx <- pre_idx[!(is.na(pre_idx))]
+        for (el in may_be_v[pre_idx]){
+                cnt = 0
+                for (i in grep(pattern=paste("\\", el, sep=""), x=inpt)){
+                        inpt <- append(x=inpt, values="\\", after=(i - 1 + cnt))
+                        cnt = cnt + 1
+                }
+        }
+  return(paste(inpt, collapse=""))
+  }
+  if (typeof(col_order) == "character"){
+    col_order <- match(x = col_order, colnames(inpt_datf))  
+  }
+  cur_search <- mapply(function(x) return(regex_spe_detect(as.character(x))), sort(x = unique(inpt_datf[, col_order]), decreasing = decreasing)[1:top_n])
+  return(inpt_datf[grep_all(pattern_v = paste0("^", cur_search, "$"), inpt_v = as.character(inpt_datf[, col_order])), ])
+}
+
+
+
 
--- README.md
+++ README.md
@@ -2253,6 +2253,71 @@ print(dynamic_idx_convertr(from_v_ids = c(1, 6), from_v_val = c("oui", "no", "ou
 ```
 
 
+# `edm_arrangr`
+
+edm_arranger
+
+
+## Description
+
+Arranges data according to the values of a variable, see examples
+
+
+## Usage
+
+```r
+edm_arrangr(inpt_datf, col_order, top_n = 10, decreasing = TRUE)
+```
+
+
+## Arguments
+
+Argument      |Description
+------------- |----------------
+`inpt_datf`     |     is the input dataframe
+`col_order`     |     is the column names or the column number of the variable that will be used to arrange data
+`top_n`     |     is the top values
+
+
+## Examples
+
+```r
+print(edm_arrangr(inpt_datf = mtcars, col_order = "wt", top_n = 15, decreasing = FALSE))
+
+mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+Lotus Europa   30.4   4  95.1 113 3.77 1.513 16.90  1  1    5    2
+Honda Civic    30.4   4  75.7  52 4.93 1.615 18.52  1  1    4    2
+Toyota Corolla 33.9   4  71.1  65 4.22 1.835 19.90  1  1    4    1
+Fiat X1-9      27.3   4  79.0  66 4.08 1.935 18.90  1  1    4    1
+Porsche 914-2  26.0   4 120.3  91 4.43 2.140 16.70  0  1    5    2
+Fiat 128       32.4   4  78.7  66 4.08 2.200 19.47  1  1    4    1
+Datsun 710     22.8   4 108.0  93 3.85 2.320 18.61  1  1    4    1
+Toyota Corona  21.5   4 120.1  97 3.70 2.465 20.01  1  0    3    1
+Mazda RX4      21.0   6 160.0 110 3.90 2.620 16.46  0  1    4    4
+Ferrari Dino   19.7   6 145.0 175 3.62 2.770 15.50  0  1    5    6
+Volvo 142E     21.4   4 121.0 109 4.11 2.780 18.60  1  1    4    2
+Mazda RX4 Wag  21.0   6 160.0 110 3.90 2.875 17.02  0  1    4    4
+Merc 230       22.8   4 140.8  95 3.92 3.150 22.90  1  0    4    2
+Ford Pantera L 15.8   8 351.0 264 4.22 3.170 14.50  0  1    5    4
+Merc 240D      24.4   4 146.7  62 3.69 3.190 20.00  1  0    4    2
+
+
+print(edm_arrangr(inpt_datf = mtcars, col_order = "wt", top_n = 10, decreasing = TRUE))
+mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+Lincoln Continental 10.4   8 460.0 215 3.00 5.424 17.82  0  0    3    4
+Chrysler Imperial   14.7   8 440.0 230 3.23 5.345 17.42  0  0    3    4
+Cadillac Fleetwood  10.4   8 472.0 205 2.93 5.250 17.98  0  0    3    4
+Merc 450SE          16.4   8 275.8 180 3.07 4.070 17.40  0  0    3    3
+Pontiac Firebird    19.2   8 400.0 175 3.08 3.845 17.05  0  0    3    2
+Camaro Z28          13.3   8 350.0 245 3.73 3.840 15.41  0  0    3    4
+Merc 450SLC         15.2   8 275.8 180 3.07 3.780 18.00  0  0    3    3
+Merc 450SL          17.3   8 275.8 180 3.07 3.730 17.60  0  0    3    3
+Duster 360          14.3   8 360.0 245 3.21 3.570 15.84  0  0    3    4
+Maserati Bora       15.0   8 301.0 335 3.54 3.570 14.60  0  1    5    8
+Dodge Challenger    15.5   8 318.0 150 2.76 3.520 16.87  0  0    3    2
+```
+
+
 # `edm_group_by1`
 
 edm_group_by1
